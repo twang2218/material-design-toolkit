601c80ec7fca1e553dfe08883835b6aef30530c2
Tao Wang <twang2218@gmail.com>
2014-10-03 16:19:11 +1000

    * Fix padding lost problem on 'ViewCompat.setBackground()';

--- lib/src/main/java/org/lab99/mdt/utils/ViewCompat.java
+++ lib/src/main/java/org/lab99/mdt/utils/ViewCompat.java
@@ -1,5 +1,6 @@
 package org.lab99.mdt.utils;
 
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.view.View;
@@ -10,11 +11,15 @@ import android.view.View;
 public final class ViewCompat extends android.support.v4.view.ViewCompat {
     @SuppressWarnings("deprecation")
     public static void setBackground(View view, Drawable drawable) {
+        //  Keep the original padding, otherwise they will be lost
+        Rect padding = new Rect(view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(), view.getPaddingBottom());
+        //  Set the background
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             view.setBackground(drawable);
         } else {
             view.setBackgroundDrawable(drawable);
         }
-
+        //  Restore the padding
+        view.setPadding(padding.left, padding.top, padding.right, padding.bottom);
     }
 }
